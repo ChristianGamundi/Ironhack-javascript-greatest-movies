33638dab99d508bcefd543ea2bceef8cca0c03c3
Gamundi Christian <20138@columbushs.com>
2019-11-20 11:44:10 -0500

    1st one

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -2,6 +2,31 @@
 
 // Iteration 1: Ordering by year - Order by year, ascending (in growing order)
 
+function orderByYear(array){
+    let copy = [...array];
+    copy.sort((a,b)=>{
+        if(a.year < b.year){
+            return -1;
+        }
+        else if(b.year < a.year){
+            return 1;
+        }
+        
+           else if(a.title < b.title){
+                return -1;
+            }
+            
+            else if(b.title < a.title){
+                return 1;
+            }
+            else{
+                return 0;
+            }
+        })
+        return array;
+    }    
+    
+
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct
 
 // Iteration 3: Alphabetic Order - Order by title and print the first 20 titles
